371a1e83c623da060035bd937f89093a1f7b274d
asuquo91 <136271445+asuquo91@users.noreply.github.com>
2023-07-13 15:00:34 +0100

    Added difficulty rating

--- CapstoneProject1_groupB/CapstoneProject1_groupB.py
+++ CapstoneProject1_groupB/CapstoneProject1_groupB.py
@@ -1,10 +1,13 @@
+
+
+
 # High Scores
 highScores = [
-  {"score":19,"Name":"Billy"},
+  {"score":60,"Name":"Billy"},
   {"score":40,"Name":"Tommy"},
-  {"score":11,"Name":"Ronny"},
-  {"score":29,"Name":"Timmy"},
-  {"score":2,"Name":"Jimmy"}
+  {"score":35,"Name":"Ronny"},
+  {"score":10,"Name":"Timmy"},
+  {"score":45,"Name":"Jimmy"}
 ]
 
 # high scores variable to sort scores in order
@@ -17,26 +20,39 @@ highScores = sorted(highScores, key=scoreSort)
 # Quiz Questions & Answers
 quizQs = [
   {
-    "question": "What year was the Premier League founded? ",
-    "answer": "1992"
+    "question": "What year was the Premier League founded?\nA: 1992 B:1985 \n",
+    "answer": "A",
+    "questionMed": "C: 1993 D: 1999 \n",
+    "questionHard": "E: 2005 F:1995\n"
   },
   {
-    "question": "Who has won the most Premier League titles? ",
-    "answer": "Manchester United"
+    "question": "Who has won the most Premier League titles?\nA: Chelsea B: Manchester United\n",
+    "answer": "b",
+    "questionMed": "C: Liverpool D: Manchester City\n",
+    "questionHard": "E: Arsenal F:Chelsea\n"
   },
   {
-    "question": "Who holds the record for most Premier League hat-tricks? ",
-    "answer": "Alan Shearer"
+    "question": "Who holds the record for most Premier League hat-tricks?\nA: Alan Shearer B: Wayne Rooney \n",
+    "answer": "a",
+    "questionMed": "C: Erling Haaland D: Didier Drogba\n",
+    "questionHard": "E: Harry Kane F: Thierry Henry\n"
   },
   {
-    "question": "Who holds the record for most Premier League wins as a manager? ",
-    "answer": "Sir Alex Ferguson"
+    "question": "Who holds the record for most Premier League wins as a manager?\nA: Jose Mourhinio B: Sir Alex Ferguson\n",
+    "answer": "b",
+    "questionMed": "C: Pep Guardiola D: Harry Rednapp\n",
+    "questionHard": "E: Arsene Wenger F: Jurgen Klopp\n"
   },
   {
-    "question": "Who holds the record for most Premier League red cards? ",
-    "answer": "Patrick Vieira"
+    "question": "Who holds the record for most Premier League red cards?\nA: Patrick Vieira B: Roy Keane \n",
+    "answer": "a",
+    "questionMed": " C: Duncan Ferguson D: Joey Barton\n",
+    "questionHard": " E: John Terry F: Vinnie Jones\n"
   },
 ]
+
+# user difficulty
+userDifficulty = "easy"
 
 # Variables for calculating prize money
 prizePerCorrectAnswer = 200000
@@ -46,22 +62,49 @@ print(
   "Welcome to Who Wants to be a Millionaire!" + "\n" +
   "There are 5 questions to answer and each correct answer is worth £200,000." + "\n" + "Good Luck!" + "\n")
 
-
+# play again status
 playAgain = "yes"
 
 # While loop start for the quiz
 while playAgain.lower() == "yes":
   userName = input("Please enter your name: " + "\n")
+  userDifficulty = input("Please enter your difficulty: Easy, Medium or Hard " + "\n")
+
   correctAnswers = 0
+  
 
   # for loop to run through the 5 questions
   for i in range(5):
-    answer = input("\n"+quizQs[i]["question"])
-    if answer.lower() == str(quizQs[i]["answer"]).lower():
-      print("\n" + "Congratulations that is correct!" + "\n")
-      correctAnswers += 10
-    else:
-      print("\n" + "That is incorrect!" + "\n")
+
+      # easy difficulty loop
+    if userDifficulty.lower() == "easy":
+      answer = input("\n"+quizQs[i]["question"])
+
+      if answer.lower() == str(quizQs[i]["answer"]).lower():
+        print("\n" + "Congratulations that is correct!" + "\n")
+        correctAnswers += 10
+      else:
+        print("\n" + "That is incorrect!" + "\n")
+
+        # Medium difficulty loop
+    elif userDifficulty.lower() == "medium":
+      answer = input("\n"+quizQs[i]["question"]+ quizQs[i]["questionMed"])
+
+      if answer.lower() == str(quizQs[i]["answer"]).lower():
+        print("\n" + "Congratulations that is correct!" + "\n")
+        correctAnswers += 10
+      else:
+        print("\n" + "That is incorrect!" + "\n")
+
+        # Hard difficulty loop
+    elif userDifficulty.lower() == "hard":
+      answer = input("\n"+quizQs[i]["question"]+ quizQs[i]["questionMed"]+ quizQs[i]["questionHard"])
+
+      if answer.lower() == str(quizQs[i]["answer"]).lower():
+        print("\n" + "Congratulations that is correct!" + "\n")
+        correctAnswers += 10
+      else:
+        print("\n" + "That is incorrect!" + "\n")    
 
   # total score and prize money print statement
   print(
