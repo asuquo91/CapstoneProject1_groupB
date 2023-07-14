e7d72672aabc8c1e98980996df3c6f6c5627969d
asuquo91 <136271445+asuquo91@users.noreply.github.com>
2023-07-14 13:40:22 +0100

    timer added

--- CapstoneProject1_groupB/CapstoneProject1_groupB.py
+++ CapstoneProject1_groupB/CapstoneProject1_groupB.py
@@ -1,13 +1,11 @@
+import time
 
-
-
-# High Scores
 highScores = [
-  {"score":60,"Name":"Billy"},
-  {"score":40,"Name":"Tommy"},
-  {"score":35,"Name":"Ronny"},
-  {"score":10,"Name":"Timmy"},
-  {"score":45,"Name":"Jimmy"}
+  {"score": 180, "Name": "Billy"},
+  {"score": 90, "Name": "Tommy"},
+  {"score": 150, "Name": "Ronny"},
+  {"score": 60, "Name": "Timmy"},
+  {"score": 80, "Name": "Jimmy"}
 ]
 
 # high scores variable to sort scores in order
@@ -23,231 +21,260 @@ quizQs = [
   {
     "question": "What year was the Premier League founded?\nA: 1992 B:1985 \n",
     "answer": "A",
-    "questionMed": "C: 1993 D: 1999 \n",
-    "questionHard": "E: 2005 F:1995\n"
+    "questionMed": "What year was the Premier League founded?\nA: 1999 B:1985 \nC: 1993 D: 1992 \n",
+    "answerMed": "D",
+    "questionHard": "What year was the Premier League founded?\nA: 1999 B:1985 \nC: 1993 D: 2000 \nE: 2005 F:1992\n",
+    "answerHard": "F"
   },
     # Question 2
   {
     "question": "Who has won the most Premier League titles?\nA: Chelsea B: Manchester United\n",
     "answer": "b",
-    "questionMed": "C: Liverpool D: Manchester City\n",
-    "questionHard": "E: Arsenal F:Chelsea\n"
+    "questionMed": "Who has won the most Premier League titles?\nA: Chelsea B: Manchester City\nC: Liverpool D: Manchester United\n",
+    "answerMed": "D",
+    "questionHard": "Who has won the most Premier League titles?\nA: Chelsea B: Manchester City\nC: Liverpool D: Manchester United\nE: Arsenal F:Chelsea\n",
+    "answerHard": "D"
   },
     # Question 3
   {
     "question": "Who holds the record for most Premier League hat-tricks?\nA: Alan Shearer B: Wayne Rooney \n",
     "answer": "a",
-    "questionMed": "C: Erling Haaland D: Didier Drogba\n",
-    "questionHard": "E: Harry Kane F: Thierry Henry\n"
+    "questionMed": "Who holds the record for most Premier League hat-tricks?\nA: Erling Haaland B: Wayne Rooney \nC: Alan Shearer D: Didier Drogba\n",
+    "answerMed": "C",
+    "questionHard": "Who holds the record for most Premier League hat-tricks?\nA: Erling Haaland B: Alan Shearer \nC: Wayne Rooney D: Didier Drogba\nE: Harry Kane F: Thierry Henry\n",
+    "answerHard": "B"
   },
     # Question 4
   {
     "question": "Who holds the record for most Premier League wins as a manager?\nA: Jose Mourhinio B: Sir Alex Ferguson\n",
     "answer": "b",
-    "questionMed": "C: Pep Guardiola D: Harry Rednapp\n",
-    "questionHard": "E: Arsene Wenger F: Jurgen Klopp\n"
+    "questionMed": "Who holds the record for most Premier League wins as a manager?\nA: Jose Mourhinio B: Harry Rednapp\nC: Pep Guardiola D: Sir Alex Ferguson\n",
+    "answerMed": "D",
+    "questionHard": "Who holds the record for most Premier League wins as a manager?\nA: Jose Mourhinio B: Harry Rednapp\nC: Pep Guardiola D: Arsene Wenger \nE: Sir Alex Ferguson F: Jurgen Klopp\n",
+    "answerHard": "E"
   },
     # Question 5
   {
     "question": "Who holds the record for most Premier League red cards?\nA: Patrick Vieira B: Roy Keane \n",
     "answer": "a",
-    "questionMed": " C: Duncan Ferguson D: Joey Barton\n",
-    "questionHard": " E: John Terry F: Vinnie Jones\n"
+    "questionMed": "Who holds the record for most Premier League red cards?\nA: Patrick Vieira B: Roy Keane \nC: Duncan Ferguson D: Joey Barton\n",
+    "answerMed": "a",
+    "questionHard": "Who holds the record for most Premier League red cards?\nA: Joey Barton  B: Roy Keane \nC: Duncan Ferguson D: Patrick Vieira\nE: John Terry F: Vinnie Jones\n",
+    "answerHard": "D"
   },
     # Question 6
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 7
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 8
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 9
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 10
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 11
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 12
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 13
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 14
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 15
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 16
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 17
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 18
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 19
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
     # Question 20
   {
     "question": "?\nA:  B:  \n",
     "answer": "a",
-    "questionMed": " C:  D: \n",
-    "questionHard": " E:  F: \n"
+    "questionMed": "?\nA:  B:  \nC:  D: \n",
+    "answerMed": "C",
+    "questionHard": "?\nA:  B:  \nC:  D: \nE:  F: \n",
+    "answerHard": "F"
   },
 ]
+
+
 
 # user difficulty
 userDifficulty = "easy"
 
-# Variables for calculating prize money
-prizePerCorrectAnswer = 200000
-
 # Welcome Message
 print("Welcome to Capstone Quiz Master, the ultimate quiz game that will test your knowledge!\nWith 20 questions of varying difficulty, this game is designed for players of all backgrounds.\n")
 print("There are three difficulty ratings: Easy, Medium, and Hard. Each question has a point value: Easy (5 points), Medium (8 points), and Hard (10 points).\n")
-print("To answer a question please type the Letter next to the answer. E.g. if the answer was C: 200 then type 'C'.\n")
+print("To answer a question, please type the letter next to the answer. E.g. if the answer was C: 200, then type 'C'.\n")
 print("Think you can conquer Capstone Quiz Master? Gather your friends, family, or fellow trivia enthusiasts and let the challenge begin!\nShow off your knowledge, aim for high scores, and become the ultimate quiz master!\n")
-
 
 # play again status
 playAgain = "yes"
 
 # While loop start for the quiz
 while playAgain.lower() == "yes":
-  userName = input("Please enter your name: " + "\n")
-  userDifficulty = input("Please enter your difficulty: Easy, Medium or Hard " + "\n")
+  userName = input("Please enter your name: ")
+  userDifficulty = input("Please enter your difficulty: Easy, Medium, or Hard: ")
 
   correctAnswers = 0
   playerScore = 0
-  
 
   # for loop to run through the 20 questions
   for i in range(20):
-
-      # easy difficulty loop
+    question = ""
+    answer_key = ""
     if userDifficulty.lower() == "easy":
-      answer = input("\n"+quizQs[i]["question"])
-
-      if answer.lower() == str(quizQs[i]["answer"]).lower():
-        print("\n" + "Congratulations that is correct!" + "\n")
-        correctAnswers += 1
-        playerScore += 5
-      else:
-        print("\n" + "That is incorrect!" + "\n")
-
-        # Medium difficulty loop
+      question = quizQs[i]["question"]
+      answer_key = quizQs[i]["answer"]
     elif userDifficulty.lower() == "medium":
-      answer = input("\n"+quizQs[i]["question"]+ quizQs[i]["questionMed"])
-
-      if answer.lower() == str(quizQs[i]["answer"]).lower():
-        print("\n" + "Congratulations that is correct!" + "\n")
-        correctAnswers += 1
-        playerScore += 8
-      else:
-        print("\n" + "That is incorrect!" + "\n")
-
-        # Hard difficulty loop
+      question = quizQs[i]["questionMed"]
+      answer_key = quizQs[i]["answerMed"]
     elif userDifficulty.lower() == "hard":
-      answer = input("\n"+quizQs[i]["question"]+ quizQs[i]["questionMed"]+ quizQs[i]["questionHard"])
+      question = quizQs[i]["questionHard"]
+      answer_key = quizQs[i]["answerHard"]
 
-      if answer.lower() == str(quizQs[i]["answer"]).lower():
-        print("\n" + "Congratulations that is correct!" + "\n")
-        correctAnswers += 1
+    
+    print("\n" + question)
+
+    start_time = time.time()
+    answer = input("Your answer: ")
+    elapsed_time = time.time() - start_time
+
+    if elapsed_time > 15:
+      print("\nTime's up! You took too long to answer.")
+    elif answer.lower() == answer_key.lower():
+      print("\nCongratulations! That is correct!")
+      correctAnswers += 1
+      if userDifficulty.lower() == "easy":
+        playerScore += 5
+      elif userDifficulty.lower() == "medium":
+        playerScore += 8
+      elif userDifficulty.lower() == "hard":
         playerScore += 10
-      else:
-        print("\n" + "That is incorrect!" + "\n")    
+    else:
+      print("\nSorry, that is incorrect.")
 
   # total score and prize money print statement
-  print(f"You scored {correctAnswers} out of 20.\n")
-  
+  print(f"\nYou scored {correctAnswers} out of 20.")
 
-  # Update high scores
-  if playerScore > highScores[0]["score"]:
-    print("Congratulations you made it onto the Leaderboard!\n")
-    highScores[0]["Name"] = userName
-    highScores[0]["score"] = playerScore
+  # Update high scores if the player's score is higher than the lowest score on the leaderboard
+  if playerScore > highScores[-1]["score"]:
+    print("\nCongratulations! You made it onto the Leaderboard!")
+    highScores[-1]["Name"] = userName
+    highScores[-1]["score"] = playerScore
     highScores = sorted(highScores, key=scoreSort)
 
   # displays updated leaderboard
-  print(f"LeaderBoard:\n1st Place = {highScores[-1]['Name']}:\t {highScores[-1]['score']}")
-  print(f"2nd Place = {highScores[-2]['Name']}:\t {highScores[-2]['score']}")
-  print(f"3rd Place = {highScores[-3]['Name']}:\t {highScores[-3]['score']}")
-  print(f"4th Place = {highScores[-4]['Name']}:\t {highScores[-4]['score']}")
-  print(f"5th Place = {highScores[-5]['Name']}:\t {highScores[-5]['score']}")
-  
+  print("\nLeaderboard:")
+  for i, score in enumerate(highScores, start=1):
+    print(f"{i} - {score['Name']}: {score['score']}")
 
   # Question to check if player wants to play again
   playAgain = input("\nWould you like to play again? (Yes or No): ")
 
-  #Displays a thank you message if the player doesn't want to play again
+# Displays a thank you message if the player doesn't want to play again
 if playAgain.upper() == "NO":
-  print("\n"+"Thank you for Playing!")
-
-
-
+  print("\nThank you for playing!")
 
